91d1ed673359ffa472a806bab4b4a0c07950ab48
remy <remynem05@gmail.com>
2017-06-28 15:51:11 +0200

    Login Activity & Sign Up Activity created with Logo

--- app/src/main/java/com/example/user/findgarage10/MainActivity.java
+++ app/src/main/java/com/example/user/findgarage10/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.user.findgarage10;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //region declaration
+    private Button btn_main_goto_login;
+    private Button btn_main_goto_signup;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        btn_main_goto_login = (Button) findViewById(R.id.main_goto_login);
+        btn_main_goto_signup = (Button) findViewById(R.id.main_goto_signup);
+
+        btn_main_goto_login.setOnClickListener(this);
+        btn_main_goto_signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.main_goto_login :
+                gotoLogin();
+                break;
+            case R.id.main_goto_signup :
+                gotoSignUp();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void gotoLogin() {
+        gotoActivity(LoginActivity.class);
+    }
+
+    private void gotoSignUp() {
+        gotoActivity(SignUpActivity.class);
+    }
+
+    private void gotoActivity(Class target){
+        Intent intent = new Intent(this, target);
+        startActivity(intent);
     }
 }
