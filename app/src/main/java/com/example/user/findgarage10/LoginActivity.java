89b5a6568929219e9a2ce5297911c90c8b14c425
remy <remynem05@gmail.com>
2017-06-28 16:49:25 +0200

    Login Activity & Sign Up Activity created with Logo

--- app/src/main/java/com/example/user/findgarage10/LoginActivity.java
+++ app/src/main/java/com/example/user/findgarage10/LoginActivity.java
@@ -1,13 +1,39 @@
 package com.example.user.findgarage10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //region declaration
+    private EditText login_et_username;
+    private EditText login_et_password;
+    private Button login_btn_login;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        login_et_username = (EditText) findViewById(R.id.login_et_username);
+        login_et_password = (EditText) findViewById(R.id.login_et_password);
+        login_btn_login = (Button) findViewById(R.id.login_btn_login);
+
+        login_btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyDevisActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
