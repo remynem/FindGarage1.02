c42bfb63d7bcc501da3232dcfff8b6e835592c37
remy <remynem05@gmail.com>
2017-06-28 22:50:28 +0200

    MyDevis Activity, List devis initialised

--- app/src/main/java/com/example/user/findgarage10/MainActivity.java
+++ app/src/main/java/com/example/user/findgarage10/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.user.findgarage10;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //region declaration
     private Button btn_main_goto_login;
@@ -24,21 +23,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initView() {
         btn_main_goto_login = (Button) findViewById(R.id.main_goto_login);
-        btn_main_goto_signup = (Button) findViewById(R.id.main_goto_signup);
+        //btn_main_goto_signup = (Button) findViewById(R.id.main_goto_signup);
 
         btn_main_goto_login.setOnClickListener(this);
-        btn_main_goto_signup.setOnClickListener(this);
+        //btn_main_goto_signup.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
-            case R.id.main_goto_login :
+        switch (v.getId()) {
+            case R.id.main_goto_login:
                 gotoLogin();
                 break;
-            case R.id.main_goto_signup :
-                gotoSignUp();
+            case R.id.main_goto_signup:
+                //gotoSignUp();
                 break;
             default:
                 break;
@@ -49,11 +48,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         gotoActivity(LoginActivity.class);
     }
 
-    private void gotoSignUp() {
+    /*private void gotoSignUp() {
         gotoActivity(SignUpActivity.class);
-    }
+    }*/
 
-    private void gotoActivity(Class target){
+    private void gotoActivity(Class target) {
         Intent intent = new Intent(this, target);
         startActivity(intent);
     }
--- app/src/main/java/com/example/user/findgarage10/MyDevisActivity.java
+++ app/src/main/java/com/example/user/findgarage10/MyDevisActivity.java
@@ -1,13 +1,33 @@
 package com.example.user.findgarage10;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MyDevisActivity extends AppCompatActivity {
+
+    //region declaration
+    private ListView listMyDevis;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_devis);
+
+        initView();
+    }
+
+    private void initView() {
+        listMyDevis = (ListView) findViewById(R.id.my_devis_lstView);
+        initList();
+
+    }
+
+    private void initList(){
+        String[] listDevis = new String[]{"Ixelles","Etterbeek","Ganshoren","Uccle","Laeken","Woluwe Saint Lambert",
+                "Neder-over-hembeek","Jette"};
+        listMyDevis.setAdapter(new ArrayAdapter<>(this,0, listDevis));
     }
 }
