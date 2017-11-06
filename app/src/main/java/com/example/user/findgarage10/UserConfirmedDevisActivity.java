6343501d06928ecc504eee7f43b0ca4d4e8a3864
remy <remynem05@gmail.com>
2017-11-06 16:08:46 +0100

    Modified to keep User Interfaces Only - Integrate FireBase Auth & Database

--- app/src/main/java/com/example/user/findgarage10/UserConfirmedDevisActivity.java
+++ app/src/main/java/com/example/user/findgarage10/UserConfirmedDevisActivity.java
@@ -3,21 +3,30 @@ package com.example.user.findgarage10;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.example.user.findgarage10.db.OfferDAO;
 import com.example.user.findgarage10.model.Offer;
-import com.example.user.findgarage10.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserConfirmedDevisActivity extends AppCompatActivity {
 
     private ListView lv_confirmed_offer;
     private Button btn_back_home;
-    private OfferDAO offerDAO;
-    private User userConnected;
+    private FirebaseDatabase database;
+    private DatabaseReference rootRef;
+    private List<Offer> offers;
 
 
     @Override
@@ -25,8 +34,11 @@ public class UserConfirmedDevisActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_confirmed_devis);
 
+
+        database = FirebaseDatabase.getInstance();
+        rootRef = database.getReference();
+
         initView();
-        initFields();
         initListView();
 
         btn_back_home.setOnClickListener(new View.OnClickListener() {
@@ -37,27 +49,50 @@ public class UserConfirmedDevisActivity extends AppCompatActivity {
         });
     }
 
-    private void goBackHome() {
-        Intent intent = new Intent(this, UserMyDevisActivity.class);
-        intent.putExtra("user", userConnected);
-        startActivity(intent);
-    }
-
-    private void initFields() {
-        Bundle bundle = getIntent().getExtras();
-        userConnected = bundle.getParcelable("user");
-    }
-
-    private void initListView() {
-        offerDAO = new OfferDAO(this);
-        offerDAO = offerDAO.openReadable();
-        Offer[] confirmedDevis = offerDAO.getConfirmedOffersForUser(userConnected.getNum_user());
-        ArrayAdapter<Offer> adapter = new ArrayAdapter<Offer>(this, android.R.layout.simple_list_item_1, android.R.id.text1, confirmedDevis);
-        lv_confirmed_offer.setAdapter(adapter);
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        initListView();
     }
 
     private void initView() {
         lv_confirmed_offer = (ListView) findViewById(R.id.lv_list_user_confirmed_offer);
         btn_back_home = (Button) findViewById(R.id.btn_confirmed_devis_back_hom);
+    }
+
+    private void initListView() {
+        offers = new ArrayList();
+        rootRef = FirebaseDatabase.getInstance().getReference("userDevis");
+        rootRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String user = snapshot.child("refUser").getValue(String.class);
+                    String status = snapshot.child("status").getValue(String.class);
+                    if(user.compareTo(FirebaseAuth.getInstance().getCurrentUser().getEmail()) == 0 && status != "pending"){
+                        String refGarage = snapshot.child("refGarage").getValue(String.class);
+                        String date = snapshot.child("date").getValue(String.class);
+                        String devisDescription = snapshot.child("devisDescription").getValue(String.class);
+
+                        Offer devis = new Offer(refGarage, date, devisDescription);
+                        devis.setConfirmed_offer(status);
+                        offers.add(devis);
+                    }
+                }
+                ArrayAdapter<Offer> adapter = new ArrayAdapter<>(UserConfirmedDevisActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, offers);
+                lv_confirmed_offer.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("val", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    private void goBackHome() {
+        Intent intent = new Intent(this, UserListNearestGarageActivity.class);
+        startActivity(intent);
     }
 }
